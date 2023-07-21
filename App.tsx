e26b3ca08e2e91d84ea37eaaac2cf868fc652a3d
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-07-21 09:55:10 +0200

    buttons, views, styles, and text fields

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [text, setText] = useState("Test Text");
+
+	return (
+		<View style={styles.view}>
+			<Text style={styles.header}>{text}</Text>
+      <TextInput onChange={() => setText("Changed")} style={styles.input}></TextInput>
+      <Button title="Change Text" onPress={() => setText("Bruh")} />
+			<StatusBar style='auto'></StatusBar>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	view: {
+		height: "100%",
+		width: "100%",
+		backgroundColor: "black",
+		display: "flex",
+		justifyContent: "center",
+		alignContent: "center",
+		alignItems: "center",
+		gap: 10,
+	},
+	header: {
+		borderRadius: 20,
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		backgroundColor: "cornflowerblue",
+		color: "#fff",
+		fontSize: 50,
+		fontWeight: "bold",
+	},
+	input: {
+    width: 250,
+    height: 30,
+    padding: 20,
+    borderColor: 'white',
+    borderRadius: 10,
+    color: 'black',
+    backgroundColor: 'white'
   },
 });
