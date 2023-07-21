29e3eb1e294cdf60ad896973a003c5435973c674
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-07-21 11:28:20 +0200

    Lesson 6: Lists and ScrollView

--- App.tsx
+++ App.tsx
@@ -1,21 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView, TextInput } from "react-native";
 import { useState } from "react";
 
 export default function App() {
-	const [text, setText] = useState("Test Text");
+	const [list, setList] = useState<{ key: string; value: string }[]>([
+		{
+			value: "test item 1",
+			key: "test key",
+		},
+	]);
 
-  function handleButtonCLick() {
-    setText("Input Value")
-  }
+	function handleTextSubmit(val: string) {
+		setList([...list, { value: val, key: `${val}-${list.length}` }]);
+	}
+
+	function handleListItemDeletion(deletionKey) {
+		const res = new Array<{ key: string; value: string }>();
+		list.forEach((item) => {
+			if (item.key != deletionKey) res.push(item);
+		});
+		setList([...res]);
+	}
 
 	return (
 		<View style={styles.view}>
-			<View style={styles.textContainer}>
-				<Text style={styles.header}>{text}</Text>
-			</View>
-			<TextInput style={styles.input} defaultValue="Input Value"  onChangeText={(val) => setText(val)} />
-			<Button title='Reset Text' onPress={() => setText("Input Value")} />
+			<Text style={styles.header}>Enter an Item</Text>
+			<TextInput
+				style={styles.input}
+        inputMode="text"
+				placeholder='Input Value'
+				onSubmitEditing={(e) => handleTextSubmit(e.nativeEvent.text)}
+			/>
+			<ScrollView style={styles.scrollView}>
+				{list.map(({ key, value }) => {
+					return (
+						<View
+							onTouchStart={() => handleListItemDeletion(key)}
+							key={key}
+							style={styles.listItemContainer}
+						>
+							<Text style={styles.listItem}>{value}</Text>
+						</View>
+					);
+				})}
+			</ScrollView>
 			<StatusBar style='auto'></StatusBar>
 		</View>
 	);
@@ -27,28 +55,40 @@ const styles = StyleSheet.create({
 		width: "100%",
 		backgroundColor: "black",
 		display: "flex",
-		justifyContent: "center",
 		alignContent: "center",
 		alignItems: "center",
 		gap: 10,
+		paddingTop: 85,
 	},
 	header: {
 		color: "#fff",
-		fontSize: 50,
+		fontSize: 20,
 		fontWeight: "bold",
 	},
 	input: {
-		width: 250,
-		height: 30,
+		width: "75%",
+		height: 35,
 		padding: 20,
 		borderColor: "white",
 		borderRadius: 10,
 		color: "black",
 		backgroundColor: "white",
 	},
-	textContainer: {
-		borderRadius: 20,
-		paddingVertical: 10,
-		paddingHorizontal: 20,
+	scrollView: {
+		width: "100%",
+	},
+	listItemContainer: {
+		paddingVertical: 12,
+		width: "75%",
+		backgroundColor: "#333",
+		borderRadius: 10,
+		marginLeft: "12.5%",
+		marginBottom: 5,
+	},
+	listItem: {
+		textAlign: "center",
+		color: "white",
+		fontSize: 15,
+		fontWeight: "bold",
 	},
 });
